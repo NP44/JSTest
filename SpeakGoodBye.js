2d0b688681ec076360d8a15b26d9f593917314b4
NP44 <neeyatipatel@yahoo.com>
2017-02-09 17:30:50 +1100

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,6 @@
+var byeSpeaker = "Good Bye";
+function speak(names) { 
+  console.log(speak + " " + names); 
+  console.log("Goodbye " + names[i]);
+ } 
+
---
+++ SpeakHello.js
@@ -0,0 +1,6 @@
+var helloSpeaker = "Hello";
+function speak(names) { 
+  console.log(speak + " " + names); 
+  console.log("Hello " + names[i]);
+ } 
+
